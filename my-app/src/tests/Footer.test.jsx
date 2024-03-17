d53c1702f7744838474fadc282bacca7b621b5ac
Michal Ceterski <michaelcet90@gmail.com>
2024-03-17 19:44:28 +0100

    third commit - Footer.test.jsx created

---
+++ my-app/src/tests/Footer.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Footer from '../components/Footer';
+
+describe('Footer component', () => {
+  test('renders footer with correct content', () => {
+    // Renderowanie komponentu stopki
+    const { getByText, getByTestId } = render(<Footer />);
+
+    // Asercja czy zawartość stopki wyświetla się poprawnie
+    expect(getByText('Linki')).toBeInTheDocument();
+    expect(getByText('Media')).toBeInTheDocument();
+    expect(getByText('Kontakt')).toBeInTheDocument();
+    expect(getByText('Strona główna')).toBeInTheDocument();
+    expect(getByText('O nas')).toBeInTheDocument();
+    expect(getByText('Kontakt')).toBeInTheDocument();
+    expect(getByTestId('tripadvisor-link')).toHaveAttribute('href', 'https://tripadvisor.com/');
+    expect(getByTestId('facebook-link')).toHaveAttribute('href', 'https://facebook.com/');
+    expect(getByTestId('instagram-link')).toHaveAttribute('href', 'https://instagram.com/');
+    expect(getByText('Adres: Al. Nieznana 5, 01-100 Tajna Góra, Nibylandia')).toBeInTheDocument();
+    expect(getByText('Tel: +425 89 123 45 67')).toBeInTheDocument();
+    expect(getByText('E-mail: info@superhotel.com')).toBeInTheDocument();
+    expect(getByText('© 2024 Super Hotel. Wszystkie prawa zastrzeżone.')).toBeInTheDocument();
+  });
+});
