a388feb5306c87d7f38a0212a514666efbee9f98
Mateusz Jamrocha <matper92@gmail.com>
2024-03-17 19:57:53 +0100

    added Galler.jsx

---
+++ my-app/src/api/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBSQ3heNYW8OHkhPd_g040aoSTefZyTWq4",
+    authDomain: "dobra-ekipa-6f3c0.firebaseapp.com",
+    projectId: "dobra-ekipa-6f3c0",
+    storageBucket: "dobra-ekipa-6f3c0.appspot.com",
+    messagingSenderId: "102294289914",
+    appId: "1:102294289914:web:a6c10bef8917dc29f63843",
+    measurementId: "G-BD8XWXYD2T"
+  };
