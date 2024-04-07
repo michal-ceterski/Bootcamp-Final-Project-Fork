1315b384d157f34ff88ecdd35a0caeb8344c39f7
Michal Ceterski <michaelcet90@gmail.com>
2024-04-07 19:04:42 +0200

    Changed handleBooking to check ID and redirect to login

--- src/components/RoomDescription.tsx
+++ src/components/RoomDescription.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import roomdata from './RoomData'
 import './Description.css'
 import { useNavigate } from "react-router-dom";
+import { UserContext } from '../auth/UserContext';
 import {Room} from './RoomData';
 
 const RoomDescription = () => {
-
     const navigate = useNavigate();
+    const { ID } = useContext(UserContext);
+
     const handleBooking = () => {
+        if (ID) {
         navigate( "/booking" );
+        } else {
+        navigate("/login", { state: { redirect: "/booking" } });
+        }
     };
 
     return (
@@ -23,7 +29,7 @@ const RoomDescription = () => {
                 a hairdryer, slippers, and bathrobes. To ensure our guests' best possible rest, all rooms are equipped with 
                 blackout curtains.</div>
             <div>
-            <div>W naszych pokojach znajdą państwo</div>
+            <div>Among our rooms you will find:</div>
             <div className="photos_container">
                 <img className="add_image" src="https://cdn.pixabay.com/photo/2017/03/05/17/39/wi-fi-2119225_1280.png"/><div>Internet w pokoju</div>
                 <img className="add_image" src="https://image.freepik.com/darmowe-ikony/lodowka_318-10736.jpg"/><div>Lodówka</div>
