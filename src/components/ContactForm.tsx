a42ec9d03aa06c2c4895c8ea40aaaf5819c695e4
Mateusz Jamrocha <matper92@gmail.com>
2024-04-07 15:15:49 +0200

    commit

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,38 +1,47 @@
 import { useState, FormEvent }  from "react"
 import './ContactForm.css'
+import { collection, addDoc, updateDoc, doc, setDoc } from 'firebase/firestore'
+import { db } from '../api/firebase'
+
+
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    subject: '',
     message: ''
   });
-// @ts-ignore
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData({...formData,[e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    await fetch('https://dobra-ekipa-6f3c0.web.app', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(formData)
-      })
-      .then((res)=>{
-        if(res.ok) {
-        alert('Message sent successfully!');
-        setFormData({ name: '', email: '', subject: '', message: '' });
-        }
-       else {
-        alert('Message sending failed.');
-      }})
-     .catch((err)=>{
-      console.log('Error sending message:', err);
-      alert('Error sending message.');
-    })
+    console.log(db)
+    await setDoc(doc(db, "cities", "LA"), {
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA"
+    });
+    
+    // await fetch('https://dobra-ekipa-6f3c0.web.app', {
+    //     method: 'POST',
+    //     headers: {'Content-Type': 'application/json'},
+    //     body: JSON.stringify(formData)
+    //   })
+    //   .then((res)=>{
+    //     if(res.ok) {
+    //     alert('Message sent successfully!');
+    //     setFormData({ name: '', email: '', message: '' });
+    //     }
+    //    else {
+    //     alert('Message sending failed.');
+    //   }})
+    //  .catch((err)=>{
+    //   console.log('Error sending message:', err);
+    //   alert('Error sending message.');
+    // })
    
   };
 
