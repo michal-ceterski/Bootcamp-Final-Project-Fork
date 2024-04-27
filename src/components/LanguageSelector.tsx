fa4ed58693dabe2838d9276e2521f1fbe251fbbe
Natalia Małecka <natalia.malecka1345@gmail.com>
2024-04-27 20:06:44 +0200

    CSS in progress - footer, language selector, map

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,35 +1,58 @@
-import React from "react";
-import { useTranslation, } from "react-i18next"
+// import React from "react";
+// import { useTranslation, } from "react-i18next"
 
-export const LanguageSelector = ()=>{
+// export const LanguageSelector = ()=>{
+//     const {t, i18n} =useTranslation()
 
-    const {t, i18n} =useTranslation()
+
     
-    if(localStorage.i18nextLng !== 'PL' ){
-        return ( 
-        <select id ='LngSelector'onChange={(e)=>{
-            i18n.changeLanguage(e.target.value)
-        }}>
-            <option value='EN' >EN</option> 
-                        <option value='PL' >PL</option> 
+//     if(localStorage.i18nextLng !== 'PL' ){
+//         return ( 
+//         <select id ='LngSelector'
+//         onChange={(e)=>{
+//             i18n.changeLanguage(e.target.value)
+//         }}>
+//             <option value='EN' >EN</option> 
+//             <option value='PL' >PL</option> 
 
-        </select>
-        )
-    }
-    else if(localStorage.i18nextLng === 'PL'){
-        return(
-            <select id ='LngSelector'onChange={(e)=>{
+
+//         </select>
+//         )
+//     }
+//     else if(localStorage.i18nextLng === 'PL'){
+//         return(
+//             <select id ='LngSelector'onChange={(e)=>{
             
-                i18n.changeLanguage(e.target.value)
+//                 i18n.changeLanguage(e.target.value)
 
-            }}> 
-            <option value='PL' >PL</option> 
-                <option value='EN' >EN</option> 
+//             }}> 
+//             <option value='PL' >PL</option> 
+//             <option value='EN' >EN</option>  
                         
-            </select>
-        )
-    }
-   
-   
+//             </select>
+//         )
+//     }
+// }
 
-}
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+export const LanguageSelector = () => {
+    const { t, i18n } = useTranslation();
+
+   
+    const defaultLanguage = 'EN';
+
+    return (
+        <select
+            id='LngSelector'
+            defaultValue={defaultLanguage} // Ustawienie defaultValue tutaj
+            onChange={(e) => {
+                i18n.changeLanguage(e.target.value);
+            }}
+        >
+            <option value='EN'>EN</option>
+            <option value='PL'>PL</option>
+        </select>
+    );
+};
